9fce6e81a4a237b67e0355caad63f6653421ea5e
youyacoder <yunliuly@cn.ibm.com>
2024-10-24 17:05:22 +0800

    feat: add unplugin-vue-components

---
+++ src/components/hello-world.vue
@@ -0,0 +1,17 @@
+<!--
+ @name: hello-world.vue
+ @description:
+ @author: 程序员优雅哥 yygcoder
+ @time: 2024/10/24 15:57
+-->
+<template>
+  <h3 class="text-blue">Hello World, {{ msg }}</h3>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  msg?: string
+}>()
+</script>
+
+<style scoped lang="scss"></style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,14 +6,9 @@
 -->
 <template>
   <div class="index">index</div>
-  <div>msg: {{ msg }}</div>
-  <div>isShow: {{ isShow }}</div>
-  <button @click="toggle()">Toggle</button>
+  <hello-world msg="message" />
 </template>
 
-<script setup lang="ts">
-const [isShow, toggle] = useToggle()
-const msg = ref('Hello world')
-</script>
+<script setup lang="ts"></script>
 
 <style scoped lang="scss"></style>
