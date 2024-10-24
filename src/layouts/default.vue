ba088ab840be26d6a0f5cfe6e28790b18d5c467e
youyacoder <yunliuly@cn.ibm.com>
2024-10-24 12:01:31 +0800

    feat: add vite-plugin-vue-layouts

---
+++ src/layouts/default.vue
@@ -0,0 +1,23 @@
+<!--
+ @name: default.vue
+ @description: default layout
+ @author: 程序员优雅哥 yygcoder
+ @time: 2024/9/10 22:49
+-->
+<template>
+  <div>
+    <header>
+      <router-link to="/" class="item">home</router-link>
+      <router-link to="/demo" class="item">demo</router-link>
+    </header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.item {
+  @apply line-height-loose mr-3 text-gray-400 mb-20px;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
 import type { App } from 'vue'
+import { setupLayouts } from 'virtual:generated-layouts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+  // routes
+  routes: setupLayouts(routes),
 })
 
 export const installRouter = (app: App) => {
