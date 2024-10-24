5b2e9a0a766d1f4ad59d15ebba7fc2611768d40f
youyacoder <yunliuly@cn.ibm.com>
2024-10-24 10:52:24 +0800

    feat: add unplugin-vue-router

---
+++ src/pages/demo.vue
@@ -0,0 +1,27 @@
+<!--
+ @name: demo.vue
+ @description: 测试页面
+ @author: 程序员优雅哥 yygcoder
+ @time: 2024/10/22 11:11
+-->
+<template>
+  <div class="demo">demo</div>
+  <h3>{{ counter }}</h3>
+  <button @click="demoStore.increment()">点击+1</button>
+
+  <div>
+    <div class="i-mdi-alarm text-orange-400" />
+    <div class="text-3xl text-red bg-green-100">Hello world</div>
+    <svg aria-hidden="true" class="svg">
+      <use href="#icon-demo" fill="red" />
+    </svg>
+  </div>
+</template>
+<script setup lang="ts">
+import { useDemoStore } from '@/stores/demo'
+import { storeToRefs } from 'pinia'
+
+const demoStore = useDemoStore()
+const { counter } = storeToRefs(demoStore)
+</script>
+<style scoped lang="scss"></style>
---
+++ src/pages/index.vue
@@ -0,0 +1,18 @@
+<!--
+ @name: index.vue
+ @description:
+ @author: 程序员优雅哥 yygcoder
+ @time: 2024/10/24 10:41
+-->
+<template>
+  <div class="index">index</div>
+</template>
+
+<script setup lang="ts">
+console.log('index')
+</script>
+
+<style scoped lang="scss">
+.index {
+}
+</style>
